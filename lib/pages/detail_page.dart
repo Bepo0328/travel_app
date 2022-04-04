771992ffa15203a477bc853365922a06cda2bd15
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-04-05 00:02:33 +0900

    TravelApp: Detail Page - Mid Section

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/misc/colors.dart';
+import 'package:travel_app/widgets/app_large_text.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({Key? key}) : super(key: key);
@@ -11,10 +13,57 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        child: Stack(),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.maxFinite,
+          height: double.maxFinite,
+          child: Stack(
+            children: [
+              Positioned(
+                left: 0,
+                right: 0,
+                child: Container(
+                  width: double.maxFinite,
+                  height: 350,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('img/mountain.jpeg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 20,
+                top: 20,
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.menu),
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 330,
+                child: Container(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                  width: MediaQuery.of(context).size.width,
+                  height: 500,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
